7238a55984cecabe282c79ecdad4de98dcee4300
Serveriii <n2jose00@students.oamk.fi>
2022-12-15 20:19:07 +0200

    Added script for results

--- Pages/Severi Jokelainen/script.js
+++ Pages/Severi Jokelainen/script.js
@@ -5,6 +5,7 @@ let modal1 = document.querySelector(".modalwrap1");
 let closer = document.querySelector(".close");
 let answerButton = document.getElementById("answerButton");
 let tryAgain = document.getElementById("tryAgain");
+const RESULTS = document.getElementById("results");
 
 percent.addEventListener("click", ()=>modal1.style.display = "block");
 // decimal.addEventListener("click", ()=>modal.style.display = "block");
@@ -28,12 +29,15 @@ function checkAnswers1() {
     let answer3 = document.getElementById("correct3");
     let answer4 = document.getElementById("correct4");
     let answerArray = [answer1,answer2,answer3,answer4]
+
+        RESULTS.innerText = " ";
+
         if (answer1.checked == true || answer2.checked == true || answer3.checked == true || answer4.checked == true) {
-            console.log("Yksi vastaus oikein neljästä");
+            RESULTS.innerText = "Yksi vastaus oikein neljästä";
         } if (answerArray.every(element => element.checked == true) == true) {
             console.log("Kaikki oikein, mahtavaa!");
         }
-    console.log(answerArray.every(element => element.checked == true));
+    
 }
 // This function clears all the answer option when clicking the "Yritä uudelleen" -button.
 function clearAnswers (){
@@ -41,7 +45,7 @@ function clearAnswers (){
     let answer2 = document.getElementsByName("answer2");
     let answer3 = document.getElementsByName("answer3");
     let answer4 = document.getElementsByName("answer4");
-
+    RESULTS.innerText = "Onnea matkaan!";
     answer1.forEach((element)=>element.checked = false);
     answer2.forEach((element)=>element.checked = false);
     answer3.forEach((element)=>element.checked = false);
